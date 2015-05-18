37a02509f41cfd15604df6e69ecef7a9c7753308
Art Mills <art@hamcois.com>
2015-05-18 02:39:43 +0000

    Cleaning up before first Heroku sync.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,40 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/rails"
+require "minitest/rails/capybara"
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+require "minitest/reporters"
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in test/support/ and its subdirectories.
+Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
+
 
 class ActiveSupport::TestCase
+  ActiveRecord::Migration.check_pending!
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Assertions
+
+end
+
